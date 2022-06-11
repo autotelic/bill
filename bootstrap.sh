a699e62cda0a88ae918363ce6d97a76482bd8434
Edward Stone <ed@autotelic.com>
2022-06-10 23:28:22 -0700

    feat: initial bootstrap.sh and install playbook

---
+++ bootstrap.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+REPO=https://github.com/autotelic/bill
+DEST=~/src/bill
+
+echo -ne "\x1b[37m"
+cat <<'EOF'
+
+   ___            __      __                  ___  _ ____
+  / _ )___  ___  / /____ / /________ ____    / _ )(_) / /
+ / _  / _ \/ _ \/ __(_-</ __/ __/ _ `/ _ \  / _  / / / /
+/____/\___/\___/\__/___/\__/_/  \_,_/ .__/ /____/_/_/_/
+                                   /_/
+
+EOF
+
+echo -ne "\x1b[0m"
+echo "
+Bill will bootstrap your machine. It will:
+
+- install Homebrew
+- install Ansible and Git with Homebrew
+- clone the bill repo
+- run the Ansible playbooks to install and setup some apps
+
+It will take a bit of time to download and install everything.
+"
+
+read -n1 -p  $'\e[0;1m Hit enter to start setup, \'n\' to cancel. Start bootstrapping? \e[2m[Y/n] ' yn
+
+if [[ $yn == [Nn] ]]; then
+  printf "\nSetup cancelled.\n"; exit;
+fi
+
+echo "Homebrew will ask for your login password."
+
+printf "\nInstalling Homebrew"
+
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+
+printf "\nInstalling Ansible and Git"
+
+brew install git ansible
+
+printf "\nCloning playbooks"
+
+if [ ! -d  $DEST ]; then
+  mkdir -p "$(dirname $DEST)"
+  git clone "$REPO" "$DEST"
+else
+  printf "\nAlready cloned, updating to latest.\n"
+  (cd $DEST && git pull --ff-only)
+fi
+
+cd $DEST && ansible-playbook -K install.yml
